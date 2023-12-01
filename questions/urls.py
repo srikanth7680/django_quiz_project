0218b0d47d10f21bcb5b81d723010e214f3162ea
srikanth <srikanth123poola@gmail.com>
2023-12-01 10:07:29 +0530

    01-12-23

--- questions/urls.py
+++ questions/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from questions.views import index,greet
+from questions.views import index,question_list,question_details,question_details_template
   ##or
 # from  .views import index,greet
+from questions import views
 
 urlpatterns = [
     path('index',index),
-    path('greet',greet)
+    path('question_list',question_list),
+    path('question/<int:question_id>',question_details),
+    path('question_temp/<int:question_id>',question_details_template)
 ]
--- questions/views.py
+++ questions/views.py
@@ -11,11 +11,31 @@ def index(request):
     # # return HttpResponse("Good morning") strings
     # return HttpResponse({"msg": "Good morning"}) ##Dict
     return HttpResponse("<html><body><h1>Hello World!</h1></body></html>") ##HTML
-def greet(request):
+def question_list(request):
     questions = Question.objects.all()
     question_list = []
     for question in questions:
         question_list.append(question.question)
     context = {'question':question_list}
     print(context)
-    return render(request,'questions/index.html',context)
+    return render(request,'questions/question_list.html',context)
+
+def question_details(request,question_id):
+    question = Question.objects.get(id=question_id)
+    question_dict = {
+        'question' : question.question,
+        'category' : question.category,
+        'added_date' : str(question.added_dt)
+    }
+    json_obj = json.dumps(question_dict)
+    return HttpResponse(json_obj)
+
+##Question_Template
+def question_details_template(request,question_id):
+    question = Question.objects.get(id=question_id)
+    context = {
+        'question' : question.question,
+        'category' : question.category,
+        'added_date' : str(question.added_dt)
+    }
+    return render(request,'questions/question_details_template.html',context)
